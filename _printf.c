61d0fff162ba14b9f05e3f2f50377434f473909d
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-10-02 07:36:19 +0000

    try another why in _printg.c

--- _printf.c
+++ _printf.c
@@ -31,28 +31,66 @@ while (*format)
 		if (*format == '\0')
 			return (-1);
 		if (*format == 'c')
+		{
 			count += _putchar_buffer(va_arg(args, int));
+			format++;
+			continue;
+		}
 		else if (*format == 's')
+		{
 			count += print_string(va_arg(args, char *));
+			format++;
+			continue;
+		}
 		else if (*format == '%')
+		{
 			count += _putchar_buffer('%');
+			format++;
+			continue;
+		}
 		else if (*format == 'd' || *format == 'i')
+		{
 			count += print_number(va_arg(args, int), flags);
+			format++;
+			continue;
+		}
 		else if (*format == 'b')
+		{
 			count += print_binary(va_arg(args, unsigned int), flags);
+		}
+
 		else if (*format == 'u') /* Here task 4 start */
-			count += print_decimal(va_arg(args, unsigned int), flags); /* u is decimal.*/
+		{
+			count += print_decimal(va_arg(args, unsigned int)
+					, flags); /* u is decimal.*/
+		}
 		else if (*format == 'o') /* o is an octal (base 8)*/
+		{
 			count += print_octal(va_arg(args, unsigned int), flags);
+		}
 		else if (*format == 'x') /* x is a number in hex in lower.*/
+		{
 			count += print_hex_lower(va_arg(args, unsigned int), flags);
+		}
 		else if (*format == 'X') /* X is a number in hex in upper.*/
+		{
 			count += print_hex_upper(va_arg(args, unsigned int), flags);
+		}
 		else if (*format == 'S')
+		{
 			count += print_S(va_arg(args, char *));
+		}
 		else if (*format == 'p')
+		{
 			count += print_pointer(va_arg(args, void *));
-		if (*format == 'l')/* here task 9 (length modifiers) */
+		}
+		else if (*format == 'd' || *format == 'i')
+		{
+			count += print_number(va_arg(args, int), flags);
+			format++;
+			continue;
+		}
+		else if (*format == 'l')/* here task 9 (length modifiers) */
 		{
 			if (*(format + 1) == 'd' || *(format + 1) == 'i')
 				count += print_long_int(va_arg(args, long int));
@@ -60,18 +98,17 @@ while (*format)
 		else if (*format == 'h')/*here task 9 */
 		{
 			if (*(format + 1) == 'd' || *(format + 1) == 'i')
+			{
 				count += print_short_int((short int)va_arg(args, int));
+			}
 		}
 		else
 		{
 			count += _putchar_buffer('%');
 			count += _putchar_buffer(*format);
+			format++;
 		}
-
 	}
-	else
-		count += _putchar_buffer(*format);
-	format++;
 }
 va_end(args);
 
